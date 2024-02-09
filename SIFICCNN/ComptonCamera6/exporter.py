96f3a710c262b0cafc09d74588366e8fa07edcbe
Ronja Hetzel <hetzel@physik.rwth-aachen.de>
2024-02-09 15:27:47 +0100

    ComptonCamera6/exporter.py: bugfix and reordering
    BUGFIX in calculation arc_unc
    
    Reording branches of tree in outputfile
     first are the reconstructed values from the NN
     followed by the values derived from them

--- SIFICCNN/ComptonCamera6/exporter.py
+++ SIFICCNN/ComptonCamera6/exporter.py
@@ -92,6 +92,7 @@ def exportCC6(filename,
     # required fields for the root file
     entries = np.sum(identified)
     zeros = np.zeros(shape=(int(entries),))
+    eventnumbers = np.arange(entries)
 
     # apply selection of post filter events
     identified = identified == 1
@@ -104,7 +105,7 @@ def exportCC6(filename,
     p_unc_x = np.array([np.sqrt(py_err[i] ** 2 + ey_err[i] ** 2) for i in range(len(py_err))])
     p_unc_y = np.array([np.sqrt(pz_err[i] ** 2 + ez_err[i] ** 2) for i in range(len(pz_err))])
     p_unc_z = np.array([np.sqrt(px_err[i] ** 2 + ex_err[i] ** 2) for i in range(len(px_err))])
-    arc_unc = np.array([0.511/np.sqrt(1-np.cos(arc[i])**2 * np.sqrt((ep_err[i]/(ep[i]**2))**2 + (ee_err[i]/(e0**2))**2)) for i in range(len(ep))])
+    arc_unc = np.array([0.511/np.sqrt(1-np.cos(arc[i])**2 * np.sqrt((ep_err[i]/(ep[i]**2))**2 + (ee_err[i]/(e0[i]**2))**2)) for i in range(len(ep))])
 
     # create root file
     if path == "":
@@ -117,15 +118,44 @@ def exportCC6(filename,
 
     # filling the branch
     # ROOT FILES ARE FILLED IN LUEBECK COORDINATE SYSTEM
-        # E1              : reconstructed electron energy in scatterer in MeV
-        # E2              : reconstructed photon energy in absorber in MeV
         # (x_1,y_1,z_1)   : reconstructed electron position in scatterer in mm
         # (x_2,y_2,z_2)   : reconstructed photon position in absorber in mm
+        # E1              : reconstructed electron energy in scatterer in MeV
+        # E2              : reconstructed photon energy in absorber in MeV
         # E0Calc          : sum of E1 and E2
         # (v_x,v_y,v_z)   : cone apex (same as (x_1,y_1,z_1))
         # (p_x,p_y,p_z)   : cone axis (calculated as difference of (x_2,y_2,z_2) and x_1,y_1,z_1))
         # arc             : cone angle in rad (calulated from E1 and E2)
-    file['ConeList'] = {'GlobalEventNumber': zeros,
+    file['ConeList'] = {'GlobalEventNumber': eventnumbers,
+                        'ClassID': zeros,
+                        'EventType': zeros,
+                        'EnergyBinID': zeros,
+                        'x_1': ey[identified],
+                        'y_1': -ez[identified],
+                        'z_1': -ex[identified],
+                        'x_1_unc': ey_err[identified],
+                        'y_1_unc': ez_err[identified],
+                        'z_1_unc': ex_err[identified],
+                        'x_2': py[identified],
+                        'y_2': -pz[identified],
+                        'z_2': -px[identified],
+                        'x_2_unc': py_err[identified],
+                        'y_2_unc': pz_err[identified],
+                        'z_2_unc': px_err[identified],
+                        'x_3': zeros,
+                        'y_3': zeros,
+                        'z_3': zeros,
+                        'x_3_unc': zeros,
+                        'y_3_unc': zeros,
+                        'z_3_unc': zeros,
+                        'E1': ee[identified],
+                        'E1_unc': ee_err[identified],
+                        'E2': ep[identified],
+                        'E2_unc': ep_err[identified],
+                        'E3': zeros,
+                        'E3_unc': zeros,
+                        'E0Calc': e0[identified],
+                        'E0Calc_unc': e0_unc[identified],
                         'v_x': ey[identified],
                         'v_y': -ez[identified],
                         'v_z': -ex[identified],
@@ -138,28 +168,8 @@ def exportCC6(filename,
                         'p_unc_x': p_unc_x[identified],
                         'p_unc_y': p_unc_y[identified],
                         'p_unc_z': p_unc_z[identified],
-                        'E0Calc': e0[identified],
-                        'E0Calc_unc': e0_unc[identified],
                         'arc': arc[identified],
-                        'arc_unc': arc_unc[identified],
-                        'E1': ee[identified],
-                        'E1_unc': ee_err[identified],
-                        'E2': ep[identified],
-                        'E2_unc': ep_err[identified],
-                        'E3': zeros,
-                        'E3_unc': zeros,
-                        'ClassID': zeros,
-                        'EventType': zeros,
-                        'EnergyBinID': zeros,
-                        'x_1': ey[identified],
-                        'y_1': -ez[identified],
-                        'z_1': -ex[identified],
-                        'x_2': py[identified],
-                        'y_2': -pz[identified],
-                        'z_2': -px[identified],
-                        'x_3': zeros,
-                        'y_3': zeros,
-                        'z_3': zeros}
+                        'arc_unc': arc_unc[identified]}
 
     # filling the branch
     file['TreeStat'] = {'StartEvent': [0],
