e20316ecf5d3e44c7df889f0e72351e5e3f48036
Philippe Clement <cflep219@hotmail.com>
2024-06-19 13:17:02 +0200

    Diverse changes, still debugging

--- SIFICCNN/downloader/dSimulationGraphSiPM.py
+++ SIFICCNN/downloader/dSimulationGraphSiPM.py
@@ -3,9 +3,25 @@ import os
 import argparse
 
 def are_connected(SiPM1, SiPM2):
-    is_y_neighbor = SiPM1.y != SiPM2.y
-    is_x_z_neighbor = abs(SiPM1.x - SiPM2.x) + abs(SiPM1.z - SiPM2.z) <=4
+    is_y_neighbor = SiPM1[1] != SiPM2[1]
+    is_x_z_neighbor = abs(SiPM1[0] - SiPM2[0]) + abs(SiPM1[2] - SiPM2[2]) <=2
     return is_x_z_neighbor and is_y_neighbor
+
+def sipm_id_to_position(sipm_id):
+    outside_check = np.greater(sipm_id, 224)
+    if np.any(outside_check == True):
+        raise ValueError("SiPMID outside detector found! ID: {} ".format(sipm_id))
+    # determine y
+    y = sipm_id // 112
+    # remove third dimension
+    sipm_id -= (y * 112)
+
+    x = sipm_id // 28
+    z = (sipm_id % 28)
+    if type(x)==int:
+        return np.array([x,y,z])        
+    else:
+        return np.array([(int(x_i), int(y_i), int(z_i)) for (x_i,y_i,z_i) in zip(x,y,z)])
 
 def dSimulation_to_GraphSiPM(simulation_data,
                            dataset_name,
@@ -42,84 +58,85 @@ def dSimulation_to_GraphSiPM(simulation_data,
             continue
         k_graphs += 1
         n_nodes += len(event.SiPMHit.SiPMId)
-        edge_counter = 0
-        for j in range(len(event.SiPMHit.SiPMId)):
-            for k in range(len(event.SiPMHit.SiPMId)):
-                if are_connected(event.SiPMHit.SiPMPosition[j], event.SiPMHit.SiPMPosition[k]):
-                    edge_counter += 1
-        m_edges += edge_counter
         n_fibre_nodes += len(event.FibreHit.FibreId)
 
     print("Total number of graphs to be created: ", k_graphs)
     print("Total number of SiPM nodes to be created: ", n_nodes)
     print("Total number of fibre nodes to be created: ", n_fibre_nodes)
-    print("Total number of edges to be created: ", m_edges)
     print("Graph features: {}".format(5))  # x, y, z, timestamp, photon count
     print("Graph targets: {}".format(2))  # Fibre energy, position
 
     # Creating final arrays
-    total_nodes = n_nodes + n_fibre_nodes
-    ary_A = np.zeros((m_edges, 2), dtype=np.int32)
+    total_nodes = n_nodes 
+    fibre_nodes = n_fibre_nodes
+    ary_A = np.zeros((224, 224), dtype=np.int8)
     ary_graph_indicator = np.zeros((total_nodes,), dtype=np.int32)
     ary_graph_labels = np.zeros((k_graphs,), dtype=np.float32)
     ary_node_attributes = np.zeros((total_nodes, 5), dtype=np.float32)  # x, y, z, timestamp, photon count
-    ary_fibre_targets = np.zeros((n_fibre_nodes, 4), dtype=np.float32)  # x, z, y, energy
+    #ary_fibre_targets = np.zeros((n_fibre_nodes, 4), dtype=np.float32)  # x, z, y, energy
+    #ary_fibre_graph_indicator = np.zeros((fibre_nodes,), dtype=np.int32)
+    fibre_tensor = np.zeros((k_graphs,55,7,2), dtype=np.float32)
+    ary_SiPM_ids = np.zeros((total_nodes), dtype=np.int8)
 
     # Main iteration over simulation data
     graph_id = 0
     node_id = 0
-    edge_id = 0
-    fibre_node_id = 0
+
+
     for i, event in enumerate(simulation_data.iterate_events(n=n)):
         if event is None:
             continue
-        n_sipm = len(event.SiPMHit.SiPMId)
-        n_fibres = len(event.FibreHit.FibreId)
+        #n_sipm = len(event.SiPMHit.SiPMId)
+        #n_fibres = len(event.FibreHit.FibreId)
+        n_sipm = 224
+        n_fibres = 385
 
         # Double iteration over SiPM nodes to determine adjacency
         for j in range(n_sipm):
-            for k in range(n_sipm):
-                if are_connected(event.SiPMHit.SiPMPosition[j], event.SiPMHit.SiPMPosition[k]):
-                    # Add the edge
-                    ary_A[edge_id, :] = [node_id + j, node_id + k]
-                    edge_id += 1
+            if i==0:
+                for k in range(n_sipm):
+                    if are_connected(sipm_id_to_position(j), sipm_id_to_position(k)):
+                        # Add the edge
+                        ary_A[j, k] = 1
 
             # Graph indicator counts which node belongs to which graph
             ary_graph_indicator[node_id] = graph_id
 
             # Collect node attributes
-            attributes = np.array([event.SiPMHit.SiPMPosition[j].x,
-                                   event.SiPMHit.SiPMPosition[j].y,
-                                   event.SiPMHit.SiPMPosition[j].z,
-                                   event.SiPMHit.SiPMTimeStamp[j],
-                                   event.SiPMHit.SiPMPhotonCount[j]])
-            ary_node_attributes[node_id, :] = attributes
+            try:
+                attributes = np.array([event.SiPMHit.SiPMPosition[j].x,
+                                    event.SiPMHit.SiPMPosition[j].y,
+                                    event.SiPMHit.SiPMPosition[j].z,
+                                    event.SiPMHit.SiPMTimeStamp[j],
+                                    event.SiPMHit.SiPMPhotonCount[j]])
+                ary_node_attributes[node_id, :] = attributes
+                ary_SiPM_ids[node_id] = event.SiPMId[j]
+                # Increment node ID
+                node_id += 1
+            except:
+                continue
 
-            # Increment node ID
-            node_id += 1
 
-        # Collect fibre node attributes and targets
+        
         for j in range(n_fibres):
-            fibre_attributes = np.array([event.FibreHit.FibrePosition[j].x,
-                                         event.FibreHit.FibrePosition[j].z,
-                                         event.FibreHit.FibrePosition[j].y,
-                                         event.FibreHit.FibreEnergy[j]])
-            ary_fibre_targets[fibre_node_id, :] = fibre_attributes
-
-            # Fibre nodes should also be included in the graph indicator
-            ary_graph_indicator[node_id] = graph_id
-            node_id += 1
-            fibre_node_id += 1
-
+            try:
+                fibre_tensor[graph_id,int((event.FibreHit.FibrePosition[j].x+55)//2),int((event.FibreHit.FibrePosition[j].z-220)//2),:]=np.array([event.FibreHit.FibrePosition[j].y, event.FibreHit.FibreEnergy[j]])
+            except:
+                continue
         # Increment graph ID
         graph_id += 1
+
+
 
     # Save arrays as .npy files
     np.save(os.path.join(path, "A.npy"), ary_A)
     np.save(os.path.join(path, "graph_indicator.npy"), ary_graph_indicator)
     np.save(os.path.join(path, "graph_labels.npy"), ary_graph_labels)
     np.save(os.path.join(path, "node_attributes.npy"), ary_node_attributes)
-    np.save(os.path.join(path, "fibre_targets.npy"), ary_fibre_targets)
+    np.save(os.path.join(path, "fibre_tensor.npy"), fibre_tensor)
+    np.save(os.path.join(path, "sipm_ids.npy"), ary_SiPM_ids)
+    """ np.save(os.path.join(path, "fibre_targets.npy"), ary_fibre_targets)
+    np.save(os.path.join(path, "fibre_indicator.npy"), ary_fibre_graph_indicator) """
 
 
 if __name__ == "__main__":
@@ -132,11 +149,11 @@ if __name__ == "__main__":
     parser.add_argument("--cs", type=str, help="Coordinate system of root file")
     args = parser.parse_args()
 
-    simulate_to_graph_data(simulation_data=args.rf,
+    """ simulate_to_graph_data(simulation_data=args.rf,
                            dataset_name=args.name,
                            path=args.path,
                            n=args.n)
-    
+     """
 
     #Coordinate system?
 
