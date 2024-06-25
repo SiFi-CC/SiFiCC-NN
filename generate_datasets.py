7aba003e6172ebbb8e2ab4e420620c85c40ab0a4
cflep <cflep219@hotail.com>
2024-06-25 18:21:28 +0200

    user friendliness

--- generate_datasets.py
+++ generate_datasets.py
@@ -8,6 +8,7 @@
 ####################################################################################################
 
 import os
+import argparse
 
 from SIFICCNN.data import RootSimulation
 from SIFICCNN.utils import parent_directory
@@ -28,13 +29,33 @@ n = 10000 #685891
 # files = ["1to1_Cluster_BP0mm_2e10protons_simV3.root"]
 #files = ["1to1_Cluster_CONT_2e10protons_simV3.root"]
 # NEW FILE 4to1_SiPM
-files = ["OptimisedGeometry_4to1_0mm_gamma_neutron_2e9_protons.root"]
 
-for file in files:
+
+def main(n, file, coordinate_system):
     root_simulation = RootSimulation(path_root + file)
     dSimulation_to_GraphSiPM(root_simulation=root_simulation,                       #Cluster statt SiPM
                                 dataset_name=root_simulation.file_name,
                                 path="",
                                 n=n,
-                                coordinate_system="AACHEN",
+                                coordinate_system=coordinate_system,
                                 energy_cut=None)
+    
+if __name__ == "__main__":
+    # configure argument parser
+    parser = argparse.ArgumentParser(description='Trainings script ECRNCluster model')
+    parser.add_argument("--n", type=int, help="Number of Events")
+    parser.add_argument("--coordinates", type=str, help="Coordinate system")
+    parser.add_argument("--file", type=str, help="File name")
+    args = parser.parse_args()
+
+    n                   = args.n if args.n is not None else None
+    coordinate_system   = args.coordinates if args.coordinates is not None else "CRACOW"
+    if args.file is not None:
+        file = args.file
+    else:
+        raise ValueError("No File specified!")
+
+    main(n, 
+         file,
+         coordinate_system
+         )
